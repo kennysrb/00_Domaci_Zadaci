fa3f097bccd6f308cc455236b9b29237b9b719a3
kennysrb <kennysrb@gmail.com>
2021-12-02 18:07:43 +0100

    v2

--- D_5_3223_NIKOLA_GLAVONJIC/script.js
+++ D_5_3223_NIKOLA_GLAVONJIC/script.js
@@ -4,17 +4,21 @@ let p = 1500;
 let m = 2000;
 let k = 550;
 
+let razlika = p + m - k;
+
 console.log(
-  `Peri je potrebno vratiti ${p - (p + m - k) / 2} dinara, a Miki ${
-    m - (p + m - k) / 2
+  `Peri je potrebno vratiti ${p - razlika / 2} dinara, a Miki ${
+    m - razlika / 2
   } dinara.`
 );
 
 // ZADATAK 2
 
 let n = 18;
-let a = 4;
+let a = 2;
 a = a + a + 2; //nova vrednost: 10
+
+console.log(`Citaocu je preostalo da procita jos: ${n - a} poglavlja.`);
 
 if (a > n / 2) {
   console.log(`Ostalo je da se procita još manje od polovine knjige`);
